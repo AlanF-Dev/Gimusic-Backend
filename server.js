41bdfc5f7a9a4dcf188c9635421bd581113eafd3
Alan <alan.fung4@gmail.com>
2023-11-18 21:33:31 -0800

    added base server

---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const PORT = process.env.PORT || 3000;
+const app = express();
+const db = require('./database/queries')      
+
+const corsOption = {
+    origin: '*',
+    credentials:true,
+    optionSuccessStatus: 200,
+};
+
+app.use(express.json())
+app.use(cors(corsOption));
+
+app.post('/signup',(req, res) => {
+
+})
+
+app.post('/login', (req, res) => {
+    
+})
