928bffb5996f19e7d5c4b321365e3d5494556607
Alan <alan.fung4@gmail.com>
2023-11-19 16:37:25 -0800

    added signup

--- database/queries.js
+++ database/queries.js
@@ -1,1 +1,24 @@
 const database = require('../databaseConnection');
+
+const createUser = async(data) => {
+    let createUserSQL = `
+        INSERT INTO user
+        (username, email, hashedPassword, user_type_id)
+        VALUES
+        (?, ?, ?, ?);
+    `
+
+    let param = [data.username, data.email, data.password, data.usertype];
+
+    try{
+        await database.query(createUserSQL, param);
+        return {success: true}
+    }
+    catch(e){
+        return {success: false}
+    }
+}
+
+module.exports = {
+    createUser
+}
--- server.js
+++ server.js
@@ -3,9 +3,11 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt');
 const PORT = process.env.PORT || 3000;
 const app = express();
-const db = require('./database/queries')      
+const db = require('./database/queries')    
+const saltRounds = 12;  
 
 const corsOption = {
     origin: '*',
@@ -16,10 +18,22 @@ const corsOption = {
 app.use(express.json())
 app.use(cors(corsOption));
 
-app.post('/signup',(req, res) => {
+app.post('/signup', async (req, res) => {
+    let hashedPassword = bcrypt.hashSync(req.body.password, saltRounds);
 
+    const result = await db.createUser({
+        username: req.body.username,
+        email: req.body.email,
+        password: hashedPassword,
+        usertype: 1
+    });
+    res.json(result)
 })
 
-app.post('/login', (req, res) => {
+app.post('/login', async (req, res) => {
     
+})
+
+app.listen(PORT, () => {
+    console.log(`APP LISTENING ON PORT: ${PORT}`)
 })
