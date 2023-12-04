da6596a7786bb5706baeb9eb46dc98623c4a703f
krispyrendang <taswinder.s@gmail.com>
2023-12-04 12:54:11 -0800

    switched to localhost

--- server.js
+++ server.js
@@ -1,116 +1,130 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const cors = require('cors');
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
 const PORT = process.env.PORT || 3000;
 const app = express();
-const db = require('./database/queries')    
-const saltRounds = 12;  
+const db = require("./database/queries");
+const saltRounds = 12;
 
 const corsOption = {
-    origin: 'https://gimusic.netlify.app',
-    // origin: 'http://localhost:5173',
-    credentials:true,
-    optionSuccessStatus: 200,
+	// origin: "https://gimusic.netlify.app",
+	origin: 'http://localhost:5173',
+	credentials: true,
+	optionSuccessStatus: 200,
 };
 
 app.use(cookieParser());
-app.use(express.json())
+app.use(express.json());
 app.use(cors(corsOption));
 
-app.get('/', (req, res) => {
-    res.json({
-        message: "GIMUSIC PROJECT"
-    })
-})
+app.get("/", (req, res) => {
+	res.json({
+		message: "GIMUSIC PROJECT",
+	});
+});
 
-app.post('/signup', async (req, res) => {
-    let hashedPassword = bcrypt.hashSync(req.body.password, saltRounds);
+app.post("/signup", async (req, res) => {
+	let hashedPassword = bcrypt.hashSync(req.body.password, saltRounds);
 
-    const result = await db.createUser({
-        username: req.body.username,
-        email: req.body.email,
-        password: hashedPassword,
-        usertype: 1
-    });
-    res.json(result);
-})
+	const result = await db.createUser({
+		username: req.body.username,
+		email: req.body.email,
+		password: hashedPassword,
+		usertype: 1,
+	});
+	res.json(result);
+});
 
-app.post('/login', async (req, res) => {
-    let results = await db.getUser({username: req.body.username});
-    if (results.user === undefined){
-        let message = "An account with that email has not been found in our records."
-        res.json({
-            success: false,
-            message: message
-        });
-    } else {
-        if ( bcrypt.compareSync(req.body.password, results.user.hashedPassword)){
-            let token = jwt.sign(
-                { username: results.user.username, email: results.user.email, usertype: results.user.user_type },
-                process.env.JWT_SECRET,
-                { expiresIn: "1h" }
-            )
-            res.cookie('token', token, { maxAge: 3600000, httpOnly: true, sameSite: 'none', secure: true })
-            res.json({
-                success: true,
-            })
-        } 
-        else {
-            let message = "Password does not match the email in our records. Try again."
-            res.json({
-                success: false,
-                message: message
-            });
-        }
-    }
-})
+app.post("/login", async (req, res) => {
+	let results = await db.getUser({ username: req.body.username });
+	if (results.user === undefined) {
+		let message =
+			"An account with that email has not been found in our records.";
+		res.json({
+			success: false,
+			message: message,
+		});
+	} else {
+		if (bcrypt.compareSync(req.body.password, results.user.hashedPassword)) {
+			let token = jwt.sign(
+				{
+					username: results.user.username,
+					email: results.user.email,
+					usertype: results.user.user_type,
+				},
+				process.env.JWT_SECRET,
+				{ expiresIn: "1h" }
+			);
+			res.cookie("token", token, {
+				maxAge: 3600000,
+				httpOnly: true,
+				sameSite: "none",
+				secure: true,
+			});
+			res.json({
+				success: true,
+			});
+		} else {
+			let message =
+				"Password does not match the email in our records. Try again.";
+			res.json({
+				success: false,
+				message: message,
+			});
+		}
+	}
+});
 
-app.post('/authenticate', async (req, res) => {
-    let token = req.cookies.token;
-    if (!token || token == undefined) {
-        res.json({
-            success: false,
-            admin: false
-        })
-    } else {
-        let data = jwt.verify(token, process.env.JWT_SECRET);
-        if (data.username == undefined || data.email == undefined || data.usertype == undefined) {
-            res.json({
-                success: false,
-                admin: false
-            })
-        } else {
-            if (data.usertype === "user") {
-                res.json({
-                    success: true,
-                    admin: false
-                })
-            } else if (data.usertype === "admin") {
-                res.json({
-                    success: true,
-                    admin: true
-                })
-            } else {
-                res.json({
-                    sucess: false,
-                    admin: false
-                })
-            }
-        }
-    }
-})
+app.post("/authenticate", async (req, res) => {
+	let token = req.cookies.token;
+	if (!token || token == undefined) {
+		res.json({
+			success: false,
+			admin: false,
+		});
+	} else {
+		let data = jwt.verify(token, process.env.JWT_SECRET);
+		if (
+			data.username == undefined ||
+			data.email == undefined ||
+			data.usertype == undefined
+		) {
+			res.json({
+				success: false,
+				admin: false,
+			});
+		} else {
+			if (data.usertype === "user") {
+				res.json({
+					success: true,
+					admin: false,
+				});
+			} else if (data.usertype === "admin") {
+				res.json({
+					success: true,
+					admin: true,
+				});
+			} else {
+				res.json({
+					sucess: false,
+					admin: false,
+				});
+			}
+		}
+	}
+});
 
-app.post('/logout', async (req, res) => {
-    res.clearCookie('token', { httpOnly: true, sameSite: 'none', secure: true });
-    res.json({
-        success: true
-    })
-})
+app.post("/logout", async (req, res) => {
+	res.clearCookie("token", { httpOnly: true, sameSite: "none", secure: true });
+	res.json({
+		success: true,
+	});
+});
 
 app.listen(PORT, () => {
-    console.log(`APP LISTENING ON PORT: ${PORT}`)
-})
+	console.log(`APP LISTENING ON PORT: ${PORT}`);
+});
